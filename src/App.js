aa445cc4a26279e5fd50307a6117fa367446a4f6
Alejandro <aromanarguello@gmail.com>
2020-07-13 14:51:12 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input id="password" />
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
-
-export default App;
